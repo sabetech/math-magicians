755c0007abddc08f7ed21d93330314ff3507d46f
Albert <albert.mensahansah@gmail.com>
2022-04-30 03:59:43 +0000

    Code refactored into functional components

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,54 +1,50 @@
+import React, { useState } from 'react';
 import './Calculator.css';
-import { Component } from 'react';
 import CalcButton from './CalcButton';
 import Display from './Display';
 import calculate from '../logic/calculate';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    /* eslint-disable */
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
+export default function Calculator() {
+  const [calculatorObj, setCalculatorObj] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
 
-    this.handleButtonPress = this.handleButtonPress.bind(this);
-  }
+  const handleButtonPress = (e) => {
+    console.log(calculatorObj);
+    const modObj = calculate(calculatorObj, e.target.textContent);
+    console.log(modObj);
+    setCalculatorObj((prevState) => ({
+      ...prevState,
+      ...modObj,
+    }));
 
-  /* eslint-disable */
-  handleButtonPress(e) {
-    this.setState((state)=> calculate(state, e.target.textContent));
-  }
+    console.log(calculatorObj);
+  };
 
-  render() {
-    return (
-      <div className="calculator parent">
-        <Display calculatorObject={this.state} />
-        <CalcButton buttonCharacter="AC" handleButtonPress={this.handleButtonPress} calculatorObject={this.state} />
-        <CalcButton buttonCharacter="+/-" handleButtonPress={this.handleButtonPress} calculatorObject={this.state} />
-        <CalcButton buttonCharacter="%" handleButtonPress={this.handleButtonPress} calculatorObject={this.state} />
-        <CalcButton buttonCharacter="&divide;" handleButtonPress={this.handleButtonPress} calculatorObject={this.state} />
-        <CalcButton buttonCharacter="7" handleButtonPress={this.handleButtonPress} calculatorObject={this.state} />
-        <CalcButton buttonCharacter="8" handleButtonPress={this.handleButtonPress} calculatorObject={this.state} />
-        <CalcButton buttonCharacter="9" handleButtonPress={this.handleButtonPress} calculatorObject={this.state} />
-        <CalcButton buttonCharacter="x" handleButtonPress={this.handleButtonPress} calculatorObject={this.state} />
-        <CalcButton buttonCharacter="4" handleButtonPress={this.handleButtonPress} calculatorObject={this.state} />
-        <CalcButton buttonCharacter="5" handleButtonPress={this.handleButtonPress} calculatorObject={this.state} />
-        <CalcButton buttonCharacter="6" handleButtonPress={this.handleButtonPress} calculatorObject={this.state} />
-        <CalcButton buttonCharacter="-" handleButtonPress={this.handleButtonPress} calculatorObject={this.state} />
-        <CalcButton buttonCharacter="1" handleButtonPress={this.handleButtonPress} calculatorObject={this.state} />
-        <CalcButton buttonCharacter="2" handleButtonPress={this.handleButtonPress} calculatorObject={this.state} />
-        <CalcButton buttonCharacter="3" handleButtonPress={this.handleButtonPress} calculatorObject={this.state} />
-        <CalcButton buttonCharacter="+" handleButtonPress={this.handleButtonPress} calculatorObject={this.state} />
-        <CalcButton buttonCharacter="." handleButtonPress={this.handleButtonPress} calculatorObject={this.state} />
-        <CalcButton buttonCharacter="=" handleButtonPress={this.handleButtonPress} calculatorObject={this.state} />
-        <CalcButton buttonCharacter="0" handleButtonPress={this.handleButtonPress} calculatorObject={this.state} />
-      </div>
-    );
-  }
+  return (
+    <div className="calculator parent">
+      <Display calculatorObject={calculatorObj} />
+      <CalcButton buttonCharacter="AC" handleButtonPress={handleButtonPress} calculatorObject={calculatorObj} />
+      <CalcButton buttonCharacter="+/-" handleButtonPress={handleButtonPress} calculatorObject={calculatorObj} />
+      <CalcButton buttonCharacter="%" handleButtonPress={handleButtonPress} calculatorObject={calculatorObj} />
+      <CalcButton buttonCharacter="&divide;" handleButtonPress={handleButtonPress} calculatorObject={calculatorObj} />
+      <CalcButton buttonCharacter="7" handleButtonPress={handleButtonPress} calculatorObject={calculatorObj} />
+      <CalcButton buttonCharacter="8" handleButtonPress={handleButtonPress} calculatorObject={calculatorObj} />
+      <CalcButton buttonCharacter="9" handleButtonPress={handleButtonPress} calculatorObject={calculatorObj} />
+      <CalcButton buttonCharacter="x" handleButtonPress={handleButtonPress} calculatorObject={calculatorObj} />
+      <CalcButton buttonCharacter="4" handleButtonPress={handleButtonPress} calculatorObject={calculatorObj} />
+      <CalcButton buttonCharacter="5" handleButtonPress={handleButtonPress} calculatorObject={calculatorObj} />
+      <CalcButton buttonCharacter="6" handleButtonPress={handleButtonPress} calculatorObject={calculatorObj} />
+      <CalcButton buttonCharacter="-" handleButtonPress={handleButtonPress} calculatorObject={calculatorObj} />
+      <CalcButton buttonCharacter="1" handleButtonPress={handleButtonPress} calculatorObject={calculatorObj} />
+      <CalcButton buttonCharacter="2" handleButtonPress={handleButtonPress} calculatorObject={calculatorObj} />
+      <CalcButton buttonCharacter="3" handleButtonPress={handleButtonPress} calculatorObject={calculatorObj} />
+      <CalcButton buttonCharacter="+" handleButtonPress={handleButtonPress} calculatorObject={calculatorObj} />
+      <CalcButton buttonCharacter="." handleButtonPress={handleButtonPress} calculatorObject={calculatorObj} />
+      <CalcButton buttonCharacter="=" handleButtonPress={handleButtonPress} calculatorObject={calculatorObj} />
+      <CalcButton buttonCharacter="0" handleButtonPress={handleButtonPress} calculatorObject={calculatorObj} />
+    </div>
+  );
 }
-
-export default Calculator;
