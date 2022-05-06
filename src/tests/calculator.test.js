b82ab34c92c4d2ed4b73eeb85dea5ada5a76a7e5
Lucas Shangrita <oustanilokman@gmail.com>
2022-05-06 16:27:54 +0300

    Test calculate logic operation (-)

--- src/tests/calculator.test.js
+++ src/tests/calculator.test.js
@@ -4,3 +4,8 @@ test('Test Calculate Logic Calculate({ total: 3, next: 6, operation: x }, =)', (
 	const obj = { total: '3', next: '6', operation: 'x' };
 	expect(calculate(obj, '=')).toEqual({ total: '18', next: null, operation: null });
 });
+
+test('Test Calculate Logic Calculate({ total: 5, next: 6, operation: - }, =)', () => {
+	const obj = { total: '5', next: '6', operation: '-' };
+	expect(calculate(obj, '=')).toEqual({ total: '-1', next: null, operation: null });
+});
