de7f9812664e3010f7a7533b7d00a19192336dc6
Albert M.Ansah <albert.mensahansah@gmail.com>
2023-01-10 11:40:53 +0000

    Update message on Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,7 @@ function Home() {
     <div>
       <h2>Welcome to our page</h2>
       <p className="paragraph">
-        Welcome to our calculator application! We are here to assist you 
-    in all your mathematical needs. Whether you're looking to perform simple 
-    arithmetic or more complex calculations, our easy-to-use interface is here 
-    to help. Please feel free to ask us any questions you may have, and we'll 
-    do our best to assist you. Let's get started!
+        Welcome to our calculator application! We are here to assist you in all your mathematical needs. Whether you\'re looking to perform simple arithmetic or more complex calculations, our easy-to-use interface is here to help. Please feel free to ask us any questions you may have, and we\'ll do our best to assist you. Let's get started!
       </p>
       <p className="paragraph">
         Use the links above to navigate to the various pages!
